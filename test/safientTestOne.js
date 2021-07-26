8f650ed42f7794c397dd2f182387158560becd44
yathishram <123yathish.r@gmail.com>
2021-07-26 19:14:26 +0530

    updates claims-js version and removes seeds in test

--- test/safientTestOne.js
+++ test/safientTestOne.js
@@ -13,55 +13,7 @@ const { JsonRpcProvider } = require('@ethersproject/providers');
 const { SignatureKind } = require('typescript');
 
 describe('Safient SDK Test Part 1', async () => {
-  // Clean up (delete users)
-  // afterEach(async () => {
-  //   const seed = new Uint8Array(randomBytes(32));
-  //   const identity = PrivateKey.fromRawEd25519Seed(Uint8Array.from(seed));
-  //   const client = await Client.withKeyInfo({
-  //     key: `${process.env.USER_API_KEY}`,
-  //     secret: `${process.env.USER_API_SECRET}`,
-  //   });
-  //   await client.getToken(identity);
-  //   const threadId = ThreadID.fromBytes(Uint8Array.from(await getThreadId()));
-  //   // DELETE : delete user A
-  //   const query = new Where('email').eq('creator@test.com');
-  //   const result = await client.find(threadId, 'Users', query);
-
-  //   if (result.length < 1) return;
-
-  //   const ids = await result.map((instance) => instance._id);
-  //   await client.delete(threadId, 'Users', ids);
-  // });
-  let creatorSeed = [
-    49, 118,  10, 108, 115, 162, 224, 171,
-   126, 224, 156,  98, 116, 222,  53,  94,
-   153, 255,  30, 159,  88, 239, 251, 229,
-    77, 132, 237, 146,  34,  42, 251,  77
- ];
-  let beneficiarySeed = [
-    99, 222, 247, 252, 228, 180, 160, 225,
-   112,   9,  41, 254,  56,  37,  28,  56,
-    56,  44,  55, 187, 139,  16, 244,  70,
-   107, 103, 205,  44,  48,  11, 127,  34
- ];
-  let guardianOneSeed = [
-    242,  13,  38, 120,  10,  85, 229,   1,
-     72, 172, 240, 121,  48, 199,  63, 252,
-    216,   2, 142, 240, 145, 151,   8,  67,
-    160,  83, 102, 222, 202, 194,  44, 185
-  ];
-  let guardianTwoSeed = [
-    190,  29, 100, 155,  95, 110,  85, 129,
-     72, 196,  77,  15, 183,  13, 145,  38,
-    116, 110, 149,  17,  95, 148, 143,  76,
-    201,   8, 233, 109,  61, 197, 245, 170
-  ];
-  let guardianThreeSeed = [
-    159, 103,  41,  70, 207, 206,  11,  58,
-    198, 237, 231, 173,   0, 240, 213, 182,
-     19,  24, 116,   6, 100,  37, 122, 249,
-     27, 194,  58,  66,  97,  86, 177,   6
-  ];
+ 
   let creator;
   let beneficiary;
   let guardianOne;
